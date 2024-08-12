a1906a05d3c082f618bf815e0313e4884927322b
Stannyut <stanley.njuguna@student.moringaschool.com>
2024-08-12 04:49:29 +0300

    Lab done

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import React from "react";
+import user from "../data/user";
+import Links from "../components/links"
 
-function About() {
+function About(bio) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      
+      {bio && <p>{user.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <h3>Links</h3>
+      <Links 
+      github={user.github} 
+      linkedin={user.linkedin} 
+      />
     </div>
   );
 }
